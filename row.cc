139e1b7e05fc41ba04594544093462e56f094cc8
darianzhang0 <77823043+darianzhang0@users.noreply.github.com>
2023-05-25 23:08:23 -0400

    Update row.cc

--- row.cc
+++ row.cc
@@ -16,8 +16,8 @@ Row::Row(std::vector<Cell*> cells) : Container{cells} {
 
 Row::~Row() { }
 
+
 void Row::notify() {
-	notifyObservers();
 	for (int i = 0; i < 9; ++i) {
 		int sum = 0;
 		for (int j = 0; j < 9; ++j) {
@@ -27,52 +27,10 @@ void Row::notify() {
 			for (int j = 0; j < 9; ++j) {
 				if (contained[j]->canBe[i] == 1) {
 					contained[j]->setValue(i + 1);
+					notifyObservers();
 					break;
 				}
 			}
 		}
 	}
 }
-/*
-void Row::notify() {
-	notifyObservers();
-	int empty = 0;
-	int cellEmpty[9] = {0};
-	for (int i = 0; i < 9; ++i) {
-		if (contained[i]->getValue() == -1) {
-			++empty;
-			cellEmpty[i] = 1;
-		}
-	}
-	for (int i = 0; i < 9; ++i) {
-		int exists = 0;
-		int emptyCell;
-		for (int j = 0; j < 9; ++j) {
-			if (cellEmpty[j] == 1) {
-				bool is_cell_empty = true;
-				for (int k = 0; k < 9; ++k) {
-					if ((contained[j]->getCol(k) == i + 1) || (contained[j]->getBox(k) == i + 1)) {
-						++exists;
-						is_cell_empty = false;
-						break;
-					} 
-				}
-				if (is_cell_empty == true) {
-					emptyCell = j;
-				}
-			}
-		}
-		if (exists == empty - 1) {
-			contained[emptyCell]->setValue(i + 1);
-		}
-	}
-}
-
-*/
-
-
-
-
-
-
-
