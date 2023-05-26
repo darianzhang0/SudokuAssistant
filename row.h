ef70a8139d413c3864cdd9010e8a9ab2f3682015
darianzhang0 <77823043+darianzhang0@users.noreply.github.com>
2023-05-25 23:08:50 -0400

    Update row.h

--- row.h
+++ row.h
@@ -5,27 +5,6 @@
 #include "container.h"
 #include "cell.h"
 
-/*
-class Container : public Subject, public Observer {
-    std::vector<Cell*> contained;
-  public:
-    virtual void notify() override;
-    Container(std::vector<Cell*>);
-    virtual ~Container();
-    // You may add additional helper functions, public or private.
-    // But you must ensure you are still implementing the observer pattern.
-};
-
-class Comic: public Book {
-  std::string hero;
- public:
-  Comic(const std::string &title, const std::string &author, int numPages, const std::string &hero);
-  bool isItHeavy() const override;
-  std::string getHero() const;
-
-  bool favourite() const override;
-}; */
-
 class Row : public Container {
 	
 	public:
